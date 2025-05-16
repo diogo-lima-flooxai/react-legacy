4c6a723064e8a96d55b6cc78f95624e561066ed5
diogo <diogo.lima.flooxai@gmail.com>
2025-05-16 10:50:59 -0300

    feat: add child components

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Primeiro, { Segundo } from "./component";
+import Family from "./family";
+import Member from "./member";
 
 ReactDOM.render(
-  <div>
-    <Primeiro />
-    <Segundo />
-  </div>,
+  <Family lastName="Silva">
+    <Member name="Guilherme" />
+  </Family>,
   document.getElementById("app")
 );
