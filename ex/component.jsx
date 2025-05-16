cb71614801cb4c0e9b84042bd7ff51f808d68817
diogo <diogo.lima.flooxai@gmail.com>
2025-05-16 10:35:45 -0300

    feat: add more than one shape to the component

--- ex/component.jsx
+++ ex/component.jsx
@@ -1,5 +1,10 @@
 import React from "react"
 
 export default props => (
-  <h1>{props.value}</h1>
+  <h1>Primeiro Component</h1>
 )
+
+export const Segundo = props => <h1>Segundo Component</h1>
+
+
+// export { Primeiro, Segundo }
